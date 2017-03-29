3ef96e6a3ab11153b7bc834c1b049a6e2b8702b9
Mathieu CARBONNEAUX <mcarbonneaux@gmail.com>
2017-03-29 23:05:17 +0200

    fix prinf %d warning

--- fcgi_protocol.c
+++ fcgi_protocol.c
@@ -475,7 +475,7 @@ int fcgi_protocol_dequeue(pool *p, fcgi_request *fr)
                         ap_log_rerror(FCGI_LOG_ERR_NOERRNO, fr->r,
                             "FastCGI: comm with server \"%s\" aborted: protocol error: "
                             "invalid FCGI_END_REQUEST size: "
-                            "%d != sizeof(FCGI_EndRequestBody)(%d)",
+                            "%d != sizeof(FCGI_EndRequestBody)(%ld)",
                             fr->fs_path, fr->dataLen, sizeof(FCGI_EndRequestBody));
                         return HTTP_INTERNAL_SERVER_ERROR;
                     }
